2f9a9fabe96af890f2c86ee49199e6ee3bf15df7
Keshav Prajapati <odinson454@gmail.com>
2025-09-25 23:47:16 +0530

    Minor changes for data

---
+++ export.py
@@ -0,0 +1,12 @@
+import json
+from pymongo import MongoClient
+
+# Connect to Atlas
+client = MongoClient("mongodb+srv://kpcode11:Kesh310509@vidtube.rqu43sk.mongodb.net")
+db = client["farmers"]
+collection = db["schemes"]
+
+# Export as JSON
+docs = list(collection.find({}))
+with open("myCollection.json", "w") as f:
+    json.dump(docs, f, default=str, indent=4)
